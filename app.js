892a7d294bf120c8e14b9c617241e19bf1b90287
dmagno00 <magno.diego00@gmail.com>
2025-11-13 17:43:49 -0300

    Refactor game logic and improve initial message display

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+    exibirTextoNaTela('p', `Escolha um número entre 1 e ${numeroLimite}`);
 }
 
 exibirMensagemInicial();
@@ -71,6 +71,7 @@ function reiniciarJogo() {
     exibirMensagemInicial();
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
+
 
 
 
